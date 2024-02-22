128921eb7968ac81e38bf7004ff361e16978237f
Yadong Zhang <yadong@onextech.com>
2024-02-22 16:20:48 +0800

    feat: added app router page.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    scrollRestoration: true,
+    // --- TODO: Remove the 2 options if possible ---
+    webpackBuildWorker: true,
+    workerThreads: true,
+    // ----------------------------------------------
+  },
 };
 
 export default nextConfig;
